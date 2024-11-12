19d5c84cf4343675fb4c016e27f465edcaaec894
vineeth2113 <152888739+vineeth2113@users.noreply.github.com>
2024-11-12 11:04:30 +0530

    Add files via upload

---
+++ web app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = []
+    with open('example.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        headers = next(csvreader)
+        for row in csvreader:
+            data.append(row)
+    return render_template('index.html', headers=headers, data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
